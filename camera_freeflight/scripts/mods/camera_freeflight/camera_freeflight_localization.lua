4cd529b20f0611002520d62885781038c0c2068d
Aussiemon <mattrohrlach+github@gmail.com>
2023-03-27 14:43:57 -0600

    Ru localization

--- camera_freeflight/scripts/mods/camera_freeflight/camera_freeflight_localization.lua
+++ camera_freeflight/scripts/mods/camera_freeflight/camera_freeflight_localization.lua
@@ -15,25 +15,31 @@ return {
   mod_name = {
     en = "Camera Freeflight",
     ["zh-cn"] = "自由飞行视角",
+    ru = "Camera Freeflight",
   },
   mod_description = {
     en = "Allows detached camera freeflight.",
     ["zh-cn"] = "允许自由飞行视角。",
+    ru = "Позволяет камере свободно летать отдельно от игрока",
   },
   cf_toggle_freeflight = {
     en = "Keybind: Toggle Freeflight",
     ["zh-cn"] = "快捷键：开关自由飞行",
+    ru = "Клавиатура: Переключение свободного полёта",
   },
   cf_toggle_freeflight_description = {
     en = "Keybind to toggle detached camera freeflight.",
     ["zh-cn"] = "用于开关自由飞行视角的按键。",
+    ru = "Клавиша или сочетание клавиш для включения/отключения свободного полета камеры.",
   },
   cf_teleport_player = {
     en = "Teleport Player to Camera",
     ["zh-cn"] = "将玩家传送到当前视角",
+    ru = "Телепортация игрока к камере",
   },
   cf_teleport_player_description = {
     en = "Enable/disable whether the player should be teleported to the camera position in freeflight.",
     ["zh-cn"] = "在自由飞行视角下，是否应该将玩家传送到视角位置。",
+    ru = "Включение/отключение телепортации игрока к камере в свободном полете.",
   },
 }
