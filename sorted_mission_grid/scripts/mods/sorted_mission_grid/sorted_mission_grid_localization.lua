d9f28d4a9632322ebc72555315a8d9739084d805
xsSplater <xssplater@gmail.com>
2023-03-28 17:05:13 +0300

    Update sorted_mission_grid_localization.lua
    Russian localization.

--- sorted_mission_grid/scripts/mods/sorted_mission_grid/sorted_mission_grid_localization.lua
+++ sorted_mission_grid/scripts/mods/sorted_mission_grid/sorted_mission_grid_localization.lua
@@ -15,9 +15,11 @@ return {
   mod_name = {
     en = "Sorted Mission Grid",
     ["zh-cn"] = "任务面板排序",
+    ru = "Sorted Mission Grid",    
   },
   mod_description = {
     en = "Sorts the mission board into a horizontal grid and adds more positions.",
     ["zh-cn"] = "以网格形式水平排列任务面板，留出更多空位。",
+    ru = "Сортирует миссии по горизонтальной сетке и добавляет больше позиций.",
   },
 }
