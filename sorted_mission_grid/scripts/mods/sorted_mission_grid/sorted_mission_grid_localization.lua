2ad53533667c9494fc70ee213fde2d586927e91a
deluxghost <deluxghost@users.noreply.github.com>
2023-03-29 16:24:13 +0800

    update zh-cn for sorted mission grid

--- sorted_mission_grid/scripts/mods/sorted_mission_grid/sorted_mission_grid_localization.lua
+++ sorted_mission_grid/scripts/mods/sorted_mission_grid/sorted_mission_grid_localization.lua
@@ -22,29 +22,38 @@ return {
   },
   smg_auto_refresh = {
     en = "Auto-Refresh",
+    ["zh-cn"] = "自动刷新",
   },
   smg_auto_refresh_description = {
     en = "Automatically refresh the mission board order when new missions arrive.",
+    ["zh-cn"] = "新任务到来时，自动刷新任务面板。",
   },
   smg_sort_order = {
     en = "Sort Order",
+    ["zh-cn"] = "排序方式",
   },
   smg_sort_order_description = {
     en = "Choose the order in which missions are displayed.",
+    ["zh-cn"] = "选择按什么顺序显示任务。",
   },
   asc_setting_text = {
     en = "Ascending",
+    ["zh-cn"] = "升序",
   },
   desc_setting_text = {
     en = "Descending",
+    ["zh-cn"] = "降序",
   },
   unsorted_setting_text = {
     en = "Unsorted",
+    ["zh-cn"] = "不排序",
   },
   smg_refresh_board = {
     en = "Keybind: Refresh Mission Board",
+    ["zh-cn"] = "快捷键：刷新任务面板",
   },
   smg_refresh_board_description = {
     en = "Hotkey to force a refresh of the mission board.",
+    ["zh-cn"] = "用于强制刷新任务面板的快捷键。",
   },
 }
