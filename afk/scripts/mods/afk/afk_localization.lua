d75bc2940f5efd856c21e94275b555eea8372644
xsSplater <xssplater@gmail.com>
2023-03-30 13:14:51 +0300

    Update afk_localization.lua
    Russian localization.

--- afk/scripts/mods/afk/afk_localization.lua
+++ afk/scripts/mods/afk/afk_localization.lua
@@ -2,17 +2,21 @@ return {
   mod_name = {
     en = "AFK",
     ["zh-cn"] = "挂机模式",
+    ru = "AFK",    
   },
   mod_description = {
     en = "Delays the server AFK timer for a set number of checks.",
     ["zh-cn"] = "延后一定次数的服务器挂机检查。",
+    ru = "Откладывает AFK-таймер сервера на заданное количество проверок.",
   },
   block_check_limit = {
     en = "Block Check Limit",
     ["zh-cn"] = "阻止检查上限",
+    ru = "Количество проверок",
   },
   block_check_limit_description = {
     en = "Maximum number of prevented checks.",
     ["zh-cn"] = "阻止挂机检查的最大次数。",
+    ru = "Максимальное количество пропущенных проверок.",
   },
 }
