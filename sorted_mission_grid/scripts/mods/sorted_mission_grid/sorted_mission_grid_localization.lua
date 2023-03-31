f4bad1cd0303bfb649b9e854a228a2109d0c3b20
xsSplater <xssplater@gmail.com>
2023-03-31 05:05:19 +0300

    Update sorted_mission_grid_localization.lua (#10)
    Russian localization.

--- sorted_mission_grid/scripts/mods/sorted_mission_grid/sorted_mission_grid_localization.lua
+++ sorted_mission_grid/scripts/mods/sorted_mission_grid/sorted_mission_grid_localization.lua
@@ -15,36 +15,47 @@ return {
   mod_name = {
     en = "Sorted Mission Grid",
     ["zh-cn"] = "任务面板排序",
+    ru = "Sorted Mission Grid",
   },
   mod_description = {
     en = "Sorts the mission board into a horizontal grid and adds more positions.",
     ["zh-cn"] = "以网格形式水平排列任务面板，留出更多空位。",
+    ru = "Сортирует миссии по горизонтальной сетке и добавляет больше позиций.",
   },
   smg_auto_refresh = {
     en = "Auto-Refresh",
+    ru = "Автообновление",
   },
   smg_auto_refresh_description = {
     en = "Automatically refresh the mission board order when new missions arrive.",
+    ru = "Автоматическре обновление порядка расположения миссий при поступлении новых.",
   },
   smg_sort_order = {
     en = "Sort Order",
+    ru = "Порядок сортировки",
   },
   smg_sort_order_description = {
     en = "Choose the order in which missions are displayed.",
+    ru = "Выберите порядок отображения миссий.",
   },
   asc_setting_text = {
     en = "Ascending",
+    ru = "По возрастанию",
   },
   desc_setting_text = {
     en = "Descending",
+    ru = "По убыванию",
   },
   unsorted_setting_text = {
     en = "Unsorted",
+    ru = "Без сортировки",
   },
   smg_refresh_board = {
     en = "Keybind: Refresh Mission Board",
+    ru = "Клавиатура: Обновить список миссий",
   },
   smg_refresh_board_description = {
     en = "Hotkey to force a refresh of the mission board.",
+    ru = "Клавиша или связка клавиш для принудительного обновления списка миссий.",
   },
 }
