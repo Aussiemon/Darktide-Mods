690653becc63e1d0902cc19718b9086975c003ed
xsSplater <xssplater@gmail.com>
2023-04-05 08:35:02 +0300

    Update prologue_localization.lua (#8)
    Russian localization.

--- prologue/scripts/mods/prologue/prologue_localization.lua
+++ prologue/scripts/mods/prologue/prologue_localization.lua
@@ -15,9 +15,11 @@ return {
   mod_name = {
     en = "Prologue",
     ["zh-cn"] = "序章",
+    ru = "Prologue",
   },
   mod_description = {
     en = "Use the /prologue command to start the Prologue.",
     ["zh-cn"] = "使用 /prologue 命令启动序章任务。",
+    ru = "Используйте команду /prologue в чате, чтобы снова пройти Пролог.",
   },
 }
