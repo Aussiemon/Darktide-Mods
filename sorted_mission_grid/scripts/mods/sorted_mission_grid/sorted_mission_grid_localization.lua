22226cd20104fb627253753004e8dd38930da10a
deluxghost <deluxghost@users.noreply.github.com>
2023-04-05 13:37:02 +0800

    update zh-cn for sorted mission grid (#9)

--- sorted_mission_grid/scripts/mods/sorted_mission_grid/sorted_mission_grid_localization.lua
+++ sorted_mission_grid/scripts/mods/sorted_mission_grid/sorted_mission_grid_localization.lua
@@ -25,37 +25,46 @@ return {
   smg_auto_refresh = {
     en = "Auto-Refresh",
     ru = "Автообновление",
+    ["zh-cn"] = "自动刷新",
   },
   smg_auto_refresh_description = {
     en = "Automatically refresh the mission board order when new missions arrive.",
     ru = "Автоматическре обновление порядка расположения миссий при поступлении новых.",
+    ["zh-cn"] = "新任务到来时，自动刷新任务面板。",
   },
   smg_sort_order = {
     en = "Sort Order",
     ru = "Порядок сортировки",
+    ["zh-cn"] = "排序方式",
   },
   smg_sort_order_description = {
     en = "Choose the order in which missions are displayed.",
     ru = "Выберите порядок отображения миссий.",
+    ["zh-cn"] = "选择按什么顺序显示任务。",
   },
   asc_setting_text = {
     en = "Ascending",
     ru = "По возрастанию",
+    ["zh-cn"] = "升序",
   },
   desc_setting_text = {
     en = "Descending",
     ru = "По убыванию",
+    ["zh-cn"] = "降序",
   },
   unsorted_setting_text = {
     en = "Unsorted",
     ru = "Без сортировки",
+    ["zh-cn"] = "不排序",
   },
   smg_refresh_board = {
     en = "Keybind: Refresh Mission Board",
     ru = "Клавиатура: Обновить список миссий",
+    ["zh-cn"] = "快捷键：刷新任务面板",
   },
   smg_refresh_board_description = {
     en = "Hotkey to force a refresh of the mission board.",
     ru = "Клавиша или связка клавиш для принудительного обновления списка миссий.",
+    ["zh-cn"] = "用于强制刷新任务面板的快捷键。",
   },
 }
