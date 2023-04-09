a973e61e3ea702a7d7bc5be3dc2de3901c8897fd
xsSplater <xssplater@gmail.com>
2023-04-09 14:40:32 +0300

    Update camera_freeflight_localization.lua (#8)

--- camera_freeflight/scripts/mods/camera_freeflight/camera_freeflight_localization.lua
+++ camera_freeflight/scripts/mods/camera_freeflight/camera_freeflight_localization.lua
@@ -15,12 +15,12 @@ return {
   mod_name = {
     en = "Camera Freeflight",
     ["zh-cn"] = "自由飞行视角",
-    ru = "Camera Freeflight",
+    ru = "Свободный полёт камеры",
   },
   mod_description = {
     en = "Allows detached camera freeflight.",
     ["zh-cn"] = "允许自由飞行视角。",
-    ru = "Позволяет камере свободно летать отдельно от игрока",
+    ru = "Camera Freeflight - Позволяет камере свободно летать отдельно от игрока",
   },
   cf_toggle_freeflight = {
     en = "Keybind: Toggle Freeflight",
@@ -30,7 +30,7 @@ return {
   cf_toggle_freeflight_description = {
     en = "Keybind to toggle detached camera freeflight.",
     ["zh-cn"] = "用于开关自由飞行视角的按键。",
-    ru = "Клавиша или сочетание клавиш для включения/отключения свободного полета камеры.",
+    ru = "Клавиша или сочетание клавиш для включения/отключения свободного полёта камеры.",
   },
   cf_teleport_player = {
     en = "Teleport Player to Camera",
@@ -40,6 +40,6 @@ return {
   cf_teleport_player_description = {
     en = "Enable/disable whether the player should be teleported to the camera position in freeflight.",
     ["zh-cn"] = "在自由飞行视角下，是否应该将玩家传送到视角位置。",
-    ru = "Включение/отключение телепортации игрока к камере в свободном полете.",
+    ru = "Включение/отключение телепортации игрока к камере в свободном полёте.",
   },
 }
